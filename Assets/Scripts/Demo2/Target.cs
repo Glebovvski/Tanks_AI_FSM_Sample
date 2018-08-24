ef3185a515934a4450e7ab0c446f1d8dfa11689f
Gleb <zadachings@gmail.com>
2018-08-24 15:53:16 +0300

    added Target script for target component in sensors demo

---
+++ Assets/Scripts/Demo2/Target.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public Transform targetMarker;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int button = 0;
+        if (Input.GetMouseButtonDown(button))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray.origin,ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                targetMarker.position = targetPosition;
+            }
+        }
+	}
+}
