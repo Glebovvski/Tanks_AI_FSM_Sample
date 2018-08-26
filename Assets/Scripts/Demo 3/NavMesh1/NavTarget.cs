bdfcd80ffe8532d34e2f3f53a7c4d889460de64a
Gleb <zadachings@gmail.com>
2018-08-26 13:45:37 +0300

    added NavTarget script

---
+++ Assets/Scripts/Demo 3/NavMesh1/NavTarget.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTarget : MonoBehaviour {
+
+    public Transform targetMarker;
+
+    private NavMeshAgent[] navAgents;
+
+    // Use this for initialization
+    void Start()
+    {
+        navAgents = FindObjectsOfType(typeof(NavMeshAgent)) as NavMeshAgent[];
+    }
+
+    void UpdateTargets(Vector3 targetPosition)
+    {
+        foreach (NavMeshAgent agent in navAgents)
+        {
+            agent.destination = targetPosition;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int button = 0;
+        if (Input.GetMouseButtonDown(button))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                UpdateTargets(targetPosition);
+                targetMarker.position = targetPosition;
+            }
+        }
+    }
+}
