581a903a4684ca9f9563eb644d95dbbadd6ebf4a
Gleb <zadachings@gmail.com>
2018-08-27 16:05:40 +0300

    added FuzzyLogic demo

---
+++ Assets/Scripts/Demo5/FuzzySample1.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuzzySample1 : MonoBehaviour {
+
+    private const string labelText = "{0} true";
+
+    public AnimationCurve critical;
+    public AnimationCurve hurt;
+    public AnimationCurve healthy;
+
+    public InputField healthInput;
+
+    public Text healthyLabel;
+    public Text hurtLabel;
+    public Text criticalLabel;
+
+    private float criticalValue = 0f;
+    private float hurtValue = 0f;
+    private float healthyValue = 0f;
+
+	// Use this for initialization
+	void Start () {
+        SetLabels();
+	}
+
+    public void EvaluateStatements()
+    {
+        if (string.IsNullOrEmpty(healthInput.text))
+            return;
+
+        float inputValue = float.Parse(healthInput.text);
+
+        healthyValue = healthy.Evaluate(inputValue);
+        hurtValue = hurt.Evaluate(inputValue);
+        criticalValue = critical.Evaluate(inputValue);
+
+        SetLabels();
+    }
+	
+    private void SetLabels()
+    {
+        healthyLabel.text = string.Format(labelText, healthyValue);
+        hurtLabel.text = string.Format(labelText, hurtValue);
+        criticalLabel.text = string.Format(labelText, criticalValue);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
