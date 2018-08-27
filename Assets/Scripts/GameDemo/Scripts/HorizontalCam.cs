92142f7d2e971d6b319da81cc0b8945987e676a6
Gleb <zadachings@gmail.com>
2018-08-27 18:53:41 +0300

    added Camera following tank script

---
+++ Assets/Scripts/GameDemo/Scripts/HorizontalCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalCam : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPosition = transform.position;
+        targetPosition.z = target.transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime);
+	}
+}
